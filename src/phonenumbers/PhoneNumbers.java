4f0122567fec9c796b81a993e704c88bc1fe434b
Israel Vasquez <israelvasquez3258@gmail.com>
2017-11-02 14:32:06 -0400

    Stack final

---
+++ src/phonenumbers/PhoneNumbers.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phonenumbers;
+
+import java.util.regex.*;
+
+/**
+ *
+ * @author ivasquez
+ */
+public class PhoneNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String phoneNumberOut;
+        String phoneNumberIn;
+        
+    }
+    
+}
