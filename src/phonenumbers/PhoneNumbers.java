53abcd283eef4d8a3339bed054743ff9a23f0ed7
Israel Vasquez <israelvasquez3258@gmail.com>
2017-11-03 14:01:52 -0400

    Stack final

--- src/phonenumbers/PhoneNumbers.java
+++ src/phonenumbers/PhoneNumbers.java
@@ -5,6 +5,7 @@
  */
 package phonenumbers;
 
+import java.util.Scanner;
 import java.util.regex.*;
 
 /**
@@ -17,8 +18,22 @@ public class PhoneNumbers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String phoneNumberOut;
-        String phoneNumberIn;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a phone number:");
+        String pNumberIn = sc.next();
+        
+        Pattern digits = Pattern.compile("([(]?)([0-9]{3})([)]?[.]?[-]?)([0-9]{3})([-]?[.]?)([0-9]{4})");
+        
+        Matcher check = digits.matcher(pNumberIn);
+        if(check.matches())
+        {
+            String F = check.group(2);
+            String S = check.group(4);
+            String T = check.group(6);
+            System.out.println("you have: " + "(" + F + ")" + S + "-" + T);
+        }
+        
+        
         
     }
     
